86d81b2d95d0e2dded0343aa77866814d9f96125
add11e2 <akshaydevadiga4@gmail.com>
2019-05-28 09:13:03 +0530

    fade not defined error resolved

--- src/MainComponents/CDPO_ACDPO/CDPO_ACDPOComponents/CDPO_ACDPOHeader.js
+++ src/MainComponents/CDPO_ACDPO/CDPO_ACDPOComponents/CDPO_ACDPOHeader.js
@@ -6,7 +6,7 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import PowerSettingsNew from "@material-ui/icons/PowerSettingsNew";
 import { Toolbar } from "@material-ui/core";
-import { fade } from "@material-ui/core/styles/colorManipulator";
+//import { fade } from "@material-ui/core/styles/colorManipulator";
 import { withStyles } from "@material-ui/core/styles";
 import firebase from "../../../Firebase";
 const styles = theme => ({
